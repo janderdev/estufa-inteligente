c0d4756d942fecbfcbe08d78cb5140b197331c16
Douglas Morais <janderdouglas2014@gmail.com>
2021-11-23 00:19:21 -0300

    passing parameters
    Implementei a passagem dos parametros iniciais da estufa de cliente.go para o servidor.go usando a o protocolo camada.go

--- cliente/cliente.go
+++ cliente/cliente.go
@@ -24,39 +24,87 @@ func main() {
 	conn, err := net.DialTCP("tcp", nil, tcpAddr)
 	checkError(err, "DialTCP")
 
-	var tempMin = -10
-	var tempMax = 5
-	var umidadeMin = 100
-	var nivelCO2Min	= 400
+	parametros := inputParametrosIniciais()
 
 	var buffer bytes.Buffer
+	buffer = converteDadosEmArrayDeBytes(parametros, buffer)
+
+	var pacote = gopacket.NewPacket(
+		buffer.Bytes(),
+		camada.ParametersLayerType,
+		gopacket.Default,
+	)
+
+	conn.Write(pacote.Data())
+	conn.Close()
+
+
+	idSensor := reqIDSensorCliente()
+	var idSensorBytes = make([]byte, 2)
+	binary.BigEndian.PutUint16(idSensorBytes, idSensor)
+	conn.Write(idSensorBytes)
+
+}
+
+func reqIDSensorCliente() uint16 {
+	var idSensor uint16
+	fmt.Println("---- BUSCANDO INFORMAÇÕES DO SENSOR ----")
+	fmt.Print("Digite o ID do Sensor: ")
+	fmt.Scanln(&idSensor)
+	return idSensor
+}
+
+func converteDadosEmArrayDeBytes(parametros struct {
+	tempMin     int16
+	tempMax     int16
+	umidadeMin  uint16
+	nivelCO2Min uint16
+}, buffer bytes.Buffer) bytes.Buffer {
 
 	var tempMinBytes = make([]byte, 4)
 	var tempMaxBytes = make([]byte, 4)
 	var umidadeMinBytes = make([]byte, 2)
 	var nivelCO2MinBytes = make([]byte, 2)
 
-	binary.BigEndian.PutUint32(tempMinBytes, uint32(tempMin))
-	binary.BigEndian.PutUint32(tempMaxBytes, uint32(tempMax))
-	binary.BigEndian.PutUint16(umidadeMinBytes, uint16(umidadeMin))
-	binary.BigEndian.PutUint16(nivelCO2MinBytes, uint16(nivelCO2Min))
+	binary.BigEndian.PutUint32(tempMinBytes, uint32(parametros.tempMin))
+	binary.BigEndian.PutUint32(tempMaxBytes, uint32(parametros.tempMax))
+	binary.BigEndian.PutUint16(umidadeMinBytes, parametros.umidadeMin)
+	binary.BigEndian.PutUint16(nivelCO2MinBytes, parametros.nivelCO2Min)
 
 	buffer.Write(tempMinBytes)
 	buffer.Write(tempMaxBytes)
 	buffer.Write(umidadeMinBytes)
 	buffer.Write(nivelCO2MinBytes)
 
-	var pacote = gopacket.NewPacket(
-		buffer.Bytes(),
-		camada.ParametersLayerType,
-		gopacket.Default,
-
-	)
-
-	conn.Write(pacote.Data())
-	conn.Close()
-
-	os.Exit(0)
+	return buffer
 }
+
+func inputParametrosIniciais() struct {
+	tempMin     int16
+	tempMax     int16
+	umidadeMin  uint16
+	nivelCO2Min uint16
+} {
+	t := struct {
+		tempMin     int16
+		tempMax     int16
+		umidadeMin  uint16
+		nivelCO2Min uint16
+	}{}
+
+	fmt.Println("---- DEFINA OS PARAMETROS INICIAIS DA ESTUFA ----")
+	fmt.Print("Temperatura Mínima: ")
+	fmt.Scanln(&t.tempMin)
+	fmt.Print("Temperatura Máxima: ")
+	fmt.Scanln(&t.tempMax)
+	fmt.Print("Umidade Mínima: ")
+	fmt.Scanln(&t.umidadeMin)
+	fmt.Print("Nível de CO2 Mínimo: ")
+	fmt.Scanln(&t.nivelCO2Min)
+	fmt.Println("-------------------------------------------------")
+
+	return t
+}
+
 
 
--- servidor/servidor.go
+++ servidor/servidor.go
@@ -29,28 +29,55 @@ func main() {
 		if err != nil {
 			return
 		}
-
-		//Fazer um if pra saber o tipo das conexão
-		//handleClient
-		result, err := ioutil.ReadAll(conn)
-		checkError(err, "ReadAll")
-
-		packet := gopacket.NewPacket(
-			result,
-			camada.ParametersLayerType,
-			gopacket.Default,
-		)
-
-		decodePacket := packet.Layer(camada.ParametersLayerType)
-
-		if decodePacket != nil {
-			fmt.Println("Pacote decodificado com sucesso!")
-			content, _ := decodePacket.(*camada.ParametersLayer)
-
-			fmt.Println("TemperaturaMin:", int32(content.TemperaturaMin))
-			fmt.Println("TemperaturaMax:", int32(content.TemperaturaMax))
-			fmt.Println("UmidadeMin:", content.UmidadeMin)
-			fmt.Println("NivelCO2Min:", content.NivelCO2Min)
-		}
+		connGetParametrosDoCliente(conn)
 	}
 }
+
+func connGetParametrosDoCliente(conn net.Conn) {
+	result, err := ioutil.ReadAll(conn)
+	checkError(err, "ReadAll")
+
+	packet := gopacket.NewPacket(
+		result,
+		camada.ParametersLayerType,
+		gopacket.Default,
+	)
+
+	decodePacket := packet.Layer(camada.ParametersLayerType)
+
+	if decodePacket != nil {
+		fmt.Println("--- PARAMETROS DA ESTUFA PASSADOS PELO CLIENTE ---")
+		content, _ := decodePacket.(*camada.ParametersLayer)
+		fmt.Println("TemperaturaMin:", int32(content.TemperaturaMin))
+		fmt.Println("TemperaturaMax:", int32(content.TemperaturaMax))
+		fmt.Println("UmidadeMin:", content.UmidadeMin)
+		fmt.Println("NivelCO2Min:", content.NivelCO2Min)
+		fmt.Println("---------------------------------------------------")
+	}
+	conn.Close()
+}
+
+
+
+/*
+func connGetSensorDoServidor(listener *net.TCPListener) {
+	conn, err := listener.Accept()
+	if err != nil {
+		return
+	}
+
+	idSensorBytes, err := ioutil.ReadAll(conn)
+	checkError(err, "ReadAll")
+
+	idSensor := binary.BigEndian.Uint16(idSensorBytes[0:2])
+
+	connGetSensorEstufa(idSensor)
+
+	conn.Close()
+}
+
+func connGetSensorEstufa(idSensor uint16) {
+
+}
+*/
+
